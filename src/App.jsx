d60b3f7a9f584556081cc314a61f7e67511812d7
Taniez <tantansug@gmail.com>
2026-07-02 18:22:48 +0700

    dfgyh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Dashboard from "./Dashboard";
 import "./index.css";
 
 export default function App() {
-  const [admin, setAdmin] = useState(null); // { name, id } หรือ null
+  const [admin, setAdmin] = useState(() => {
+    const name = sessionStorage.getItem("adminName");
+    const id   = sessionStorage.getItem("adminId");
+    if (name && id) return { name, id };
+    return null;
+  });
 
   return admin ? (
     <Dashboard admin={admin} onLogout={() => setAdmin(null)} />
